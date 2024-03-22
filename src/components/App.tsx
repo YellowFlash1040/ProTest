0ca51e2a1d73c354b0f0f7bed1f9ba7bb7964efa
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-03-22 17:35:11 +0100

    I did basic application routing in App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,27 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Layout } from "../components";
-import MainPage from "../pages/MainPage/MainPage";
+import {
+  AuthPage,
+  ContactsPage,
+  MainPage,
+  Results,
+  Test,
+  UsefulInfo,
+} from "../pages";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<MainPage />}></Route>
+        <Route index element={<MainPage />} />
+        <Route path="auth" element={<AuthPage />} />
+        <Route path="contacts" element={<ContactsPage />} />
+        <Route path="results" element={<Results />} />
+        <Route path="test" element={<Test />} />
+        <Route path="useful-info" element={<UsefulInfo />} />
       </Route>
+      <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
   );
 }
