4e5b44e6e19052b486e375c2a9165bc54da79934
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-04-10 01:36:29 +0200

    I want to think I made /useful-info page

--- src/pages/UsefulInfo/UsefulInfo.tsx
+++ src/pages/UsefulInfo/UsefulInfo.tsx
@@ -1,5 +1,60 @@
+import s from "./UsefulInfo.module.css"
+
 const UserfulInfo = () => {
-  return <div>UserfulInfo</div>
+  return (
+    <section className={s.section}>
+      <div className={s.infoBlockContainer}>
+        <h2 className={s.usefulLiteratureLabel}>Useful literature</h2>
+        <ol className={s.usefulLiteratureList}>
+          <li>Testing dot.com Savin.</li>
+          <li>A mental hospital in the hands of patients.</li>
+          <li>Scrum. J. Sutherland.</li>
+        </ol>
+      </div>
+
+      <div className={s.infoBlockContainer}>
+        <h2 className={s.usefulResourcesLabel}>Useful literature</h2>
+        <ol className={s.usefulResourcesList}>
+          <li>
+            &#160;
+            <a className={s.resourceLink} href='https://dou.ua' target='_blank'>
+              dou.ua
+            </a>
+          </li>
+          <li>
+            &#160;
+            <a
+              className={s.resourceLink}
+              href='https://habr.com'
+              target='_blank'
+            >
+              Habr
+            </a>
+          </li>
+          <li>
+            &#160;
+            <a
+              className={s.resourceLink}
+              href='https://facebook.com/QA'
+              target='_blank'
+            >
+              facebook.com/QA
+            </a>
+          </li>
+          <li>
+            &#160;
+            <a
+              className={s.resourceLink}
+              href='https://goit.ua'
+              target='_blank'
+            >
+              goit.ua
+            </a>
+          </li>
+        </ol>
+      </div>
+    </section>
+  )
 }
 
 export default UserfulInfo
